2009a950ac7aca636e6bed4204cfc111e81b762c
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-01-17 10:58:52 -0500

    feat: accounts table

--- src/lib/inspect/account.ts
+++ src/lib/inspect/account.ts
@@ -1,9 +1,9 @@
 import ora from "ora";
 import CliTable3 from "cli-table3";
-import picocolors from "picocolors";
 import { warnMessage } from "@/lib/logs";
 import { InspectorBaseArgs } from "@/types/inspect";
-import { Address, GetTransactionApi, Signature } from "@solana/web3.js";
+import { Address, GetAccountInfoApi } from "@solana/web3.js";
+import { lamportsToSol } from "@/lib/web3";
 
 export async function inspectAddress({
   rpc,
@@ -23,15 +23,40 @@ export async function inspectAddress({
 
     if (!account) throw "Account not found";
 
+    const overviewTable = buildAccountOverview(account);
+
     // we must the spinner before logging any thing or else the spinner will be displayed as frozen
     spinner.stop();
 
-    console.log(account);
-
-    // console.log(overviewTable.toString());
-    // console.log(accountsTable.toString());
+    console.log(overviewTable.toString());
   } catch (err) {
     spinner.stop();
     warnMessage(err);
   }
+}
+function buildAccountOverview({
+  value: account,
+}: ReturnType<GetAccountInfoApi["getAccountInfo"]>): CliTable3.Table {
+  const table = new CliTable3({
+    head: ["Account Overview"],
+    style: {
+      head: ["green"],
+    },
+  });
+
+  table.push(["Owner", account.owner]);
+  table.push(["Executable", account.executable]);
+  table.push([
+    "Balance (lamports)",
+    new Intl.NumberFormat().format(account.lamports),
+  ]);
+  table.push(["Balance (sol)", lamportsToSol(account.lamports)]);
+  table.push([
+    "Space",
+    // @ts-expect-error - known bug: `space` is missing from the type
+    // see: https://github.com/anza-xyz/solana-web3.js/issues/12
+    new Intl.NumberFormat().format(account.space) + " bytes",
+  ]);
+
+  return table;
 }
