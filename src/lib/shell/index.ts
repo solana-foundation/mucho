09c63b2e0148f732d7d4a6838d98dabea9aee874
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-03-07 13:02:22 -0500

    fix: build

--- src/lib/shell/index.ts
+++ src/lib/shell/index.ts
@@ -163,8 +163,6 @@ export function shellExecInSession({
   args = undefined,
   outputOnly,
 }: ShellExecInSessionArgs): ChildProcess | void {
-  console.log(""); // spacer in the terminal
-
   if (outputOnly) {
     args = args || [];
     args.unshift(command);
